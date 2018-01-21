33f854f267264bd985eeeb297d647c090dc0a50b
shawnjun21 <35420577+shawnjun21@users.noreply.github.com>
2018-01-20 22:32:28 -1000

    Add files via upload

--- goldfish.js
+++ goldfish.js
@@ -1,90 +1,138 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const token = "ToKen";
+const token = "NDA0NDg3NTc5NDkzMzM1MDQx.DUWnLQ.lIYDX1zZknizZoWioF8qOByMXmo";
 
-var pile = [
-":hearts: 1",
-":hearts: 2",
-":hearts: 3",
-":hearts: 4",
-":hearts: 5",
-":hearts: 6",
-":hearts: 7",
-":hearts: 8",
-":hearts: 9",
-":hearts: 10",
-":hearts: Jack",
-":hearts: Queen",
-":hearts: King",
-":diamonds: 1",
-":diamonds: 2",
-":diamonds: 3",
-":diamonds: 4",
-":diamonds: 5",
-":diamonds: 6",
-":diamonds: 7",
-":diamonds: 8",
-":diamonds: 9",
-":diamonds: 10",
-":diamonds: Jack",
-":diamonds: Queen",
-":diamonds: King",
-":spades: 1",
-":spades: 2",
-":spades: 3",
-":spades: 4",
-":spades: 5",
-":spades: 6",
-":spades: 7",
-":spades: 8",
-":spades: 9",
-":spades: 10",
-":spades: Jack",
-":spades: Queen",
-":spades: King",
-":clubs: 1",
-":clubs: 2",
-":clubs: 3",
-":clubs: 4",
-":clubs: 5",
-":clubs: 6",
-":clubs: 7",
-":clubs: 8",
-":clubs: 9",
-":clubs: 10",
-":clubs: Jack",
-":clubs: Queen",
-":clubs: King",
+//All the cards in the deck where suit: 0 is clubs, 1 is diamonds, 2 is spades, 3 is hearts. 
+//value: 0 is Ace, 10 is Jack, 11 is Queen, 12 is King
+//owner: 0 means the card is still in the deck, otherwise it will contain a discord ID
+var all_cards = [
+{suit:0, value:0, owner:0},
+{suit:0, value:1, owner:0},
+{suit:0, value:2, owner:0},
+{suit:0, value:3, owner:0},
+{suit:0, value:4, owner:0},
+{suit:0, value:5, owner:0},
+{suit:0, value:6, owner:0},
+{suit:0, value:7, owner:0},
+{suit:0, value:8, owner:0},
+{suit:0, value:9, owner:0},
+{suit:0, value:10, owner:0},
+{suit:0, value:11, owner:0},
+{suit:0, value:12, owner:0},
+{suit:1, value:0, owner:0},
+{suit:1, value:1, owner:0},
+{suit:1, value:2, owner:0},
+{suit:1, value:3, owner:0},
+{suit:1, value:4, owner:0},
+{suit:1, value:5, owner:0},
+{suit:1, value:6, owner:0},
+{suit:1, value:7, owner:0},
+{suit:1, value:8, owner:0},
+{suit:1, value:9, owner:0},
+{suit:1, value:10, owner:0},
+{suit:1, value:11, owner:0},
+{suit:1, value:12, owner:0},
+{suit:2, value:0, owner:0},
+{suit:2, value:1, owner:0},
+{suit:2, value:2, owner:0},
+{suit:2, value:3, owner:0},
+{suit:2, value:4, owner:0},
+{suit:2, value:5, owner:0},
+{suit:2, value:6, owner:0},
+{suit:2, value:7, owner:0},
+{suit:2, value:8, owner:0},
+{suit:2, value:9, owner:0},
+{suit:2, value:10, owner:0},
+{suit:2, value:11, owner:0},
+{suit:2, value:12, owner:0},
+{suit:3, value:0, owner:0},
+{suit:3, value:1, owner:0},
+{suit:3, value:2, owner:0},
+{suit:3, value:3, owner:0},
+{suit:3, value:4, owner:0},
+{suit:3, value:5, owner:0},
+{suit:3, value:6, owner:0},
+{suit:3, value:7, owner:0},
+{suit:3, value:8, owner:0},
+{suit:3, value:9, owner:0},
+{suit:3, value:10, owner:0},
+{suit:3, value:11, owner:0},
+{suit:3, value:12, owner:0},
 ];
+//0 = game did not start, 1 = game started
 var gamestart = 0
 ;
-var players = [
+//number of players in the beginning
+var players = 0
+;
+//Usernames of players
+var ids = [
+
 ];
-var player1 = [
-];
-var player2 = [
-];
-var player3 = [
-];
-var player4 = [
-];
+var test = 0
+;
+//does various things
+var counter = 0
 bot.on("message", message =>
 {
     var txt = message.content.split(" ");
-
     if (txt[0].toLowerCase() == "-play")
-    {
-        if (players.length < 5)
-        {
-            if (gamestart = 0)
-            {
-                players.find(function(element)
-                {return element == message.author.username}
-                if
+    {   //# of players detection
+        if (players < 4)
+        {   //game status detection
+            if (gamestart == 0)
+            {   //remember that username only return the name, the last four digits #abcd
+                var detectid = ids.indexOf(message.author.username)
+                if (detectid == -1)
+                {
+                    message.channel.send("Sucessfully joined the game!")
+                    //for some odd reason, without this wait code, everything happens simultaneously and does not work. :P 
+                    ids.push("" + message.author.username + "")
+                    message.channel.send(ids[0])
+                    setTimeout(wait, 1000)
+                    function wait() {
+                        players += 1
+                    }
+                    //Will not do anything until players join and someone says -start
+                    bot.on("message", message =>
+                    {
+                        var txt = message.content.split(" ");
+                        if (txt[0].toLowerCase() == "-start")
+                        {
+                            message.channel.send("Starting the game with " + players + " players...")
+                            //This part will repeat the product of # of players and 5 times.
+                            for(p = 0; p < players; p++)
+                            {
+                                for(i = 0; i < 5; i++)
+                                {
+                                    function guarantee(){
+                                    var pickedcard = Math.floor(Math.random() * 52);
+                                    message.channel.send(pickedcard)
+                                    if(all_cards[pickedcard].owner == 0)
+                                    {
+                                        message.channel.send("done!")
+                                    }
+                                    else
+                                    {
+                                        message.channel.send("repeating")
+                                        guarantee()
+                                    }  
+                                }
+                                }
+                                counter += 1
+                            }
+                            //message.channel.send(test)
+                        }
+                    });
+                }
+                else
+                {
+                    message.channel.send("You're already in the game!")
+                }
             }
             else
             {
-
+                message.channel.send("The game has already started.")
             }
         }
         else
